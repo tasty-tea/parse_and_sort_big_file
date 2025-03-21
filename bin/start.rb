47a68bf391e945ab76f94ee3f9a5464e7f71d3d5
tasty-tea <mrarttermos@gmail.com>
2025-03-21 08:35:40 +0600

    ExternalSorting is now a class, further refactoring

--- bin/start.rb
+++ bin/start.rb
@@ -8,5 +8,5 @@ CHUNK_SIZE = 1000
 input_filename = 'input_data.txt'
 output_filename = "new_output.txt"
 
-ExternalSorting.sort_large_file(input_filename: input_filename, output_filename: output_filename,
-                                chunk_size: CHUNK_SIZE)
+ExternalSorting.call(input_filename: input_filename, output_filename: output_filename,
+                     chunk_size: CHUNK_SIZE)
--- lib/external_sorting.rb
+++ lib/external_sorting.rb
@@ -1,14 +1,26 @@
 require_relative 'custom_sort'
 require_relative 'transaction'
 
-module ExternalSorting
-  module_function
+class ExternalSorting
+  attr_accessor :input_filename, :output_filename, :chunk_size
 
-  def sort_large_file(input_filename:, output_filename:, chunk_size: 1000)
-    tempfiles = create_sorted_tempfile_chunks(input_filename, chunk_size)
-
-    create_output_file(output_filename, tempfiles)
+  def self.call(input_filename:, output_filename:, chunk_size: 1000)
+    new(input_filename, output_filename, chunk_size).call
   end
+
+  def initialize(input_filename, output_filename, chunk_size)
+    @input_filename = input_filename
+    @output_filename = output_filename
+    @chunk_size = chunk_size
+  end
+
+  def call
+    tempfiles = create_sorted_tempfile_chunks(@input_filename, @chunk_size)
+
+    create_output_file(@output_filename, tempfiles)
+  end
+
+  private
 
   def create_sorted_tempfile_chunks(input_filename, chunk_size, sorter: CustomSort, parser: Transaction)
     tempfiles = []
@@ -25,14 +37,14 @@ module ExternalSorting
     tempfiles
   end
 
-  def create_output_file(output_filename, input_files)
+  def create_output_file(output_filename, input_files, parser: Transaction)
     current_lines = input_files.map { |file| file.gets&.chomp }
 
     # Используем алгоритм External sorting
 
     File.open(output_filename, "w") do |out|
       while current_lines.any? { |line| !line.nil? }
-        max_index = find_max_index(current_lines, Transaction)
+        max_index = find_max_index(current_lines, parser)
         out.puts current_lines[max_index]
 
         next_line = input_files[max_index].gets
